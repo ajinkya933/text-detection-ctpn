0349ec0cf89c4a75db886d6ec77cc81e3976a8cb
sleeping barber <ajinkyabobade93@gmail.com>
2019-08-09 17:45:07 +0530

    Update denormalise-multi.py

--- label_img_to_input_data/denormalise-multi.py
+++ label_img_to_input_data/denormalise-multi.py
@@ -5,30 +5,57 @@ import shutil
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element
 
+
+def extract_coords(list_of_xmls):
+	for item in list_of_xmls:
+		#print(item)
+
+	#xml_raw = open("demofile.txt", "w") 
+	#l=[]
+
+	    with open(item) as f: root = ET.parse(f)
+	    xml_raw = open(item+".txt", "w")
+
+	    
+	    for obj in root.findall('object'):
+	    	#print(obj)
+	    	bndbox: Element = obj.find('bndbox')
+	    	name = obj.find('name').text
+	    	xmin, xmax, ymin, ymax = [int(bndbox.find(x).text) for x in ['xmin', 'xmax', 'ymin', 'ymax']]
+	    	coords = [(x, y) for x in [xmin, xmax] for y in [ymin, ymax]]
+	    #l.append(coords)
+	    	print(coords, ','+name)
+	    	xml_raw.write(str(coords)+str(',english')+'\n')
+	#print(l, name)
+
+
+def remove_char(list_of_txt):
+	for item in list_of_txt:
+		output = open("temp.txt", "w")
+		with open(item, 'r+') as f:
+			for line in f.readlines():
+				line = line.replace("[", "")
+				line = line.replace("(", "")
+				line = line.replace(")", "")
+				line = line.replace("]", "")
+				line = line.replace(" ", "")
+				output.write(line)
+		output.close()
+		os.remove(item) # remove old file
+		os.rename("temp.txt",item) # rename as old file
+
+
+		#print(item)
+
+
+
 path = os.getcwd()
 print(path)
 os.chdir(path)
 newpath=os.path
 
 list_of_xmls = glob.glob(path+"//*.xml")
-#name='demofile'
+extract_coords(list_of_xmls)
 
-for item in list_of_xmls:
-#	print(item)
-
-#xml_raw = open("demofile.txt", "w") 
-#l=[]
-
-    with open(item) as f: root = ET.parse(f)
-    xml_raw = open(item+".txt", "w") 
-    for obj in root.findall('object'):
-        bndbox: Element = obj.find('bndbox')
-        name = obj.find('name').text
-        xmin, xmax, ymin, ymax = [int(bndbox.find(x).text) for x in ['xmin', 'xmax', 'ymin', 'ymax']]
-        coords = [(x, y) for x in [xmin, xmax] for y in [ymin, ymax]]
-    #l.append(coords)
-
-        print(coords, ','+name)
-        xml_raw.write(str(coords)+str(',english')+'\n')
-#print(l, name)
-
+list_of_txt = glob.glob(path+"//*.txt")
+remove_char(list_of_txt)
